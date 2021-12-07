4b2a5b257e7eab9d700c52a9de79e1d624d07535
azer <azerprojects@gmail.com>
2021-12-07 16:21:12 +0700

    ubuntu(config.sh): create directory before symlink

--- ubuntu/config.sh
+++ ubuntu/config.sh
@@ -3,11 +3,11 @@
 DOTFILE_DIR="$(pwd)/dotfiles"
 
 ln -vsf -t "$HOME" "$DOTFILE_DIR/.tmux.conf.local" "$DOTFILE_DIR/.zshrc" "$DOTFILE_DIR/servers.yml"
-ln -vsf -t "$HOME/.config/nvim" "$DOTFILE_DIR/init.vim"
-ln -vsf -t "$HOME/.config/alacritty" "$DOTFILE_DIR/alacritty.yml"
-ln -vsf -t "$HOME/.config/rustfmt" "$DOTFILE_DIR/rustfmt.toml"
-ln -vsf "$DOTFILE_DIR/efmconfig.yaml" "$HOME/.config/efm-langserver/config.yaml"
-ln -vsf -t "$HOME/.local/share/fonts/" "$DOTFILE_DIR/NerdFonts"
+mkdir -p "$HOME/.config/nvim" && ln -vsf -t "$HOME/.config/nvim" "$DOTFILE_DIR/init.vim"
+mkdir -p "$HOME/.config/alacritty" && ln -vsf -t "$HOME/.config/alacritty" "$DOTFILE_DIR/alacritty.yml"
+mkdir -p "$HOME/.config/rustfmt" && ln -vsf -t "$HOME/.config/rustfmt" "$DOTFILE_DIR/rustfmt.toml"
+mkdir -p "$HOME/.config/efm-langserver" && ln -vsf "$DOTFILE_DIR/efmconfig.yaml" "$HOME/.config/efm-langserver/config.yaml"
+mkdir -p "$HOME/.local/share/fonts/" && ln -vsf -t "$HOME/.local/share/fonts/" "$DOTFILE_DIR/NerdFonts"
 fc-cache -f -v
 
 # optional if you use guake
