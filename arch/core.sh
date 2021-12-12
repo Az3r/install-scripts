addff2e04c4e5c9832343f753f3635570b7aa52a
azer <azerprojects@gmail.com>
2021-12-12 11:55:22 +0700

    arch(core.sh): polybar and i3

--- arch/core.sh
+++ arch/core.sh
@@ -9,11 +9,21 @@ git clone https://aur.archlinux.org/yay.git
 cd yay || exit
 yes | makepkg -si
 cd ..
+rm -rf yay
+
+# displaye server
+sudo pacman -S --noconfirm xorg-server xterm xorg-xrandr autorandr
 
 # display manager
-sudo pacman -S --noconfirm xorg-server xterm i3-wm
 sudo pacman -S --noconfirm lightdm lightdm-webkit-theme-litarvan lightdm-webkit2-greeter
-
-# systemctl enable lightdm
+systemctl enable lightdm
 # note: config /etc/lightdm/lightdm.conf greeter-session
 # note: /etc/lightdm/lightdm-webkit2-greeter.conf set theme to litarvan
+
+# window manager
+printf "n\nN\ny\n" | yay -S polybar
+sudo pacman -S --noconfirm i3-wm polybar i3status
+mkdir -p ~/.config/polybar
+cp /usr/share/doc/polybar/config ~/.config/polybar/config
+print "#!/bin/bash\nkillall -q polybar\npolybar mybar 2>&1 | tee -a /tmp/polybar.log & disown" >~/.config/polybar/launch.sh
+echo "exec_always --no-startup-id $HOME/.config/polybar/launch.sh" >>~/.config/i3/config
