a30cb5aefb577c5d5ce12ade735ed5c565229b25
azer <azerprojects@gmail.com>
2021-12-25 08:33:10 +0700

    ubuntu: does not change shell while installing

--- ubuntu/core.sh
+++ ubuntu/core.sh
@@ -6,7 +6,7 @@ sudo apt-get update && sudo apt-get upgrade -y
 timedatectl set-local-rtc 1 --adjust-system-clock
 
 # install core libraries
-sudo apt-get -y install trash-cli curl wget git xclip firefox neovim apt-transport-https ca-certificates gnupg-agent software-properties-common file build-essential gnome-disk-utility gparted unzip python3-pip dosfstools libreadline-dev
+sudo apt-get -y install trash-cli curl wget git xclip firefox apt-transport-https ca-certificates gnupg-agent software-properties-common file build-essential gnome-disk-utility gparted unzip python3-pip dosfstools libreadline-dev
 
 # unikey
 sudo add-apt-repository -y ppa:ubuntu-vn/ppa
@@ -20,7 +20,7 @@ sudo apt-get -y install zsh
 sh 0>/dev/null -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
-chsh -s "$(which zsh)" # set to default shell
+# chsh -s "$(which zsh)" # set to default shell
 
 # terminal emulator
 sudo apt-get -y install guake                          # dropdown terminal
