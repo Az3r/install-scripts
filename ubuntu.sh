01418c64b0277c574714d512b20ae1455d87a85d
azer <azerprojects@gmail.com>
2021-12-25 08:22:32 +0700

    ubuntu: update startup file

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-chmod +x core.sh programming.sh personal.sh config.sh
-./ubuntu/core.sh
-./ubuntu/programming.sh
-./ubuntu/personal.sh
-./config.sh
+INSTALL_DIRECTORY="./ubuntu"
+chmod +x "$INSTALL_DIRECTORY/core.sh"
+chmod +x "$INSTALL_DIRECTORY/programming.sh"
+chmod +x "$INSTALL_DIRECTORY/personal.sh"
+chmod +x "$INSTALL_DIRECTORY/config.sh"
+
+./INSTALL_DIRECTORY/core.sh
+./INSTALL_DIRECTORY/programming.sh
+./INSTALL_DIRECTORY/personal.sh
+./INSTALL_DIRECTORY/config.sh
